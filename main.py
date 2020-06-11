93ae8cfde54321fb0236f4b595fab212d65b6a78
elise0605 <elise.tobiasson@dagbladet.no>
2020-06-11 14:47:28 +0200

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-# Called once per day (6:30am by default).
-# Invokes weather forecast and sudoku-gfx scripts.
-def daily():
-  GPIO.output(ledPin, GPIO.HIGH)
-  subprocess.call(["python", "forecast.py"])
-  subprocess.call(["python", "sudoku-gfx.py"])
-  subprocess.call(["python", "lunch.py"])
-  GPIO.output(ledPin, GPIO.LOW)
